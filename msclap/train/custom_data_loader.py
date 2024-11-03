7906fe950e00e839edd0469e075ce6a5f52dbbe9
Lee-daeho <zmnh2003@gmail.com>
2024-11-03 06:59:39 +0000

    Feat: Training 코드 작성

---
+++ msclap/train/custom_data_loader.py
@@ -0,0 +1,67 @@
+import os
+import torch
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+import torchaudio
+import pandas as pd
+from tqdm import tqdm
+
+
+class CustomAudioTextDataset(Dataset):
+    def __init__(self, audio_directory, csv_path):
+        self.audio_dir = audio_directory
+        self.csv_path = csv_path
+        self.data = self._load_data()
+
+    def _load_data(self):
+        df = pd.read_csv(self.csv_path)
+        data = []
+        for _, row in tqdm(
+            df.iterrows(), total=df.shape[0], desc=f"Loading {self.csv_path} data"
+        ):
+            audio_file = row["filename"].split(".")[0] + ".wav"
+            wav_path = os.path.join(self.audio_dir, audio_file)
+
+            if os.path.exists(wav_path):
+                data.append({"audio_path": wav_path, "transcript": row["text"]})
+            else:
+                print(f"Warning: Audio file not found: {wav_path}")
+
+        return data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        item = self.data[idx]
+        waveform, sample_rate = torchaudio.load(item["audio_path"])
+        if waveform.shape[0] > 1:
+            waveform = torch.mean(waveform, dim=0, keepdim=True)
+
+        return {
+            "audio_path": item["audio_path"],
+            "waveform": waveform[0],
+            "sample_rate": sample_rate,
+            "transcript": item["transcript"],
+        }
+
+    def get_audio_sample_rate(self, idx):
+        audio_path = self.data[idx]["audio_path"]
+        metadata = torchaudio.info(audio_path)
+        return metadata.sample_rate
+
+
+def collate_fn(batch):
+    max_length = max(item["waveform"].shape[0] for item in batch)
+
+    waveforms = [
+        F.pad(item["waveform"], (0, max_length - item["waveform"].shape[0]))
+        for item in batch
+    ]
+
+    return {
+        "audio_path": [item["audio_path"] for item in batch],
+        "waveform": torch.stack(waveforms).float(),
+        "sample_rate": [item["sample_rate"] for item in batch],
+        "transcript": [item["transcript"] for item in batch],
+    }
---
+++ msclap/train/trainer.py
@@ -0,0 +1,100 @@
+import torch
+import wandb
+from utils import process_batch
+
+class CLAPTrainer:
+    def __init__(self, model, optimizer, scaler, device):
+        self.model = model
+        self.optimizer = optimizer
+        self.scaler = scaler
+        self.device = device
+
+    def train_epoch(self, clap_wrapper, dataloader):
+        total_loss = 0
+        torch.cuda.empty_cache()
+
+        for batch in dataloader:
+            loss, _, _ = process_batch(
+                clap_wrapper,
+                self.device,
+                batch,
+            )
+
+            self.scaler.scale(loss).backward()
+
+            self.scaler.unscale_(self.optimizer)
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
+            self.scaler.step(self.optimizer)
+            self.scaler.update()
+            self.optimizer.zero_grad()
+
+            total_loss += loss.item()
+
+        avg_loss = total_loss / len(dataloader)
+        return avg_loss
+
+    def validate(self, clap_wrapper, dataloader):
+        torch.cuda.empty_cache()
+        total_loss = 0
+        total_correct = 0
+        total_samples = 0
+
+        with torch.no_grad():
+            for batch in dataloader:
+                loss, audio_embeddings, text_embeddings = process_batch(
+                    clap_wrapper,
+                    self.device,
+                    batch,
+                )
+                total_loss += loss.item()
+
+                # Accuracy calculation
+                batch_size = audio_embeddings.shape[0]
+                for i in range(batch_size):
+                    sample_audio_embedding = audio_embeddings[i].unsqueeze(0)
+                    similarities = F.cosine_similarity(
+                        sample_audio_embedding, text_embeddings
+                    )
+                    predicted_index = similarities.argmax().item()
+                    if predicted_index == i:
+                        total_correct += 1
+                total_samples += batch_size
+
+        avg_loss = total_loss / len(dataloader)
+        accuracy = total_correct / total_samples
+        return avg_loss, accuracy
+
+    def train(self, clap_wrapper, train_dataloader, validation_dataloader, 
+              num_epochs):
+
+        for epoch in range(num_epochs):
+            train_loss = self.train_epoch(
+                clap_wrapper,
+                train_dataloader,
+            )
+
+            val_loss, val_accuracy = self.validate(
+                clap_wrapper,
+                validation_dataloader
+            )
+
+            print(
+                f"Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss}, Val Loss: {val_loss}, Val Accuracy: {val_accuracy:.4f}"
+            )
+
+            wandb.log(
+                {
+                    "epoch": epoch + 1,
+                    "train_loss": train_loss,
+                    "val_loss": val_loss,
+                    "val_accuracy": val_accuracy,
+                }
+            )
+
+            if epoch == num_epochs - 1:
+                torch.save(
+                    self.model.state_dict(),
+                    f"/home/rag/para-sentence-rag/src/embedding_models/msclap/params/model_epoch_{epoch+1}_{wandb.run.name}_{clap_wrapper.args.text_model}-{clap_wrapper.args.audioenc_name}.pth",
+                )
+
+        return self.model
